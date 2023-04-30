9b9af3c5036c7fe4e83ceb58f9c286a051122d4c
hayun0909 <lin5478@naver.com>
2023-04-30 20:39:42 +0900

    update run-adapter-wo-pretraining.sh

--- run-adapter-wo-pretraining.sh
+++ run-adapter-wo-pretraining.sh
@@ -1,5 +1,5 @@
-python experiments/adapter-wo-pretraining.py --exp_name wo_pretrain --batch_size 8 --num_fold 0 --num_epochs 40
-python adapter-wo-pretraining.py --exp_name wo_pretrain --batch_size 8 --num_fold 1 --num_epochs 40
-python adapter-wo-pretraining.py --exp_name wo_pretrain --batch_size 8 --num_fold 2 --num_epochs 40
-python adapter-wo-pretraining.py --exp_name wo_pretrain --batch_size 8 --num_fold 3 --num_epochs 40
-python adapter-wo-pretraining.py --exp_name wo_pretrain --batch_size 8 --num_fold 4 --num_epochs 40
+python continual_learning_adapter_wo_pretraining.py --num_fold 0 --batch_size 8 --num_epochs 40
+python continual_learning_adapter_wo_pretraining.py --num_fold 1 --batch_size 8 --num_epochs 40
+python continual_learning_adapter_wo_pretraining.py --num_fold 2 --batch_size 8 --num_epochs 40
+python continual_learning_adapter_wo_pretraining.py --num_fold 3 --batch_size 8 --num_epochs 40
+python continual_learning_adapter_wo_pretraining.py --num_fold 4 --batch_size 8 --num_epochs 40
